161f98dd7ce11c53ccbca3977558901b847273cb
Aleksandr Schigol <aleksandr@schigol.com>
2014-05-21 20:00:17 +0300

    add missing twitter_url reader

--- lib/echowrap/urls.rb
+++ lib/echowrap/urls.rb
@@ -3,7 +3,7 @@ require 'echowrap/base'
 module Echowrap
   class Urls < Echowrap::Base
     attr_reader :amazon_url, :itunes_url, :lastfm_url, :mb_url, :myspace_url, 
-                :official_url, :wikipedia_url
+                :official_url, :wikipedia_url, :twitter_url
     
   end
 end
